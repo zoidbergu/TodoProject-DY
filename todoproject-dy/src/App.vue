58729cf41b91ebd1a4b22e85d6c56bbf60d14fa3
hyeonwoo <hyeonu030334@gmail.com>
2023-05-14 13:53:25 +0900

    todo add

--- todoproject-dy/src/App.vue
+++ todoproject-dy/src/App.vue
@@ -1,31 +1,36 @@
+// App.vue
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-  <p>안녕하세요</p>
+  <Title name="TO-DO" />
+  <!-- @<customEvent 이름>="실행되는 method이름" -->
+  <ToDoInput @add-to-do="addToDo" />
+  <ToDoList :todos="todos" />
 </template>
 
+<script>
+import Title from './components/Title.vue'
+import ToDoInput from './components/ToDoInput.vue'
+import ToDoList from './components/ToDoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Title,
+    ToDoInput,
+    ToDoList
+  },
+  data() { 
+    return {
+      todos: ['과제하기','숙제하기','청소하기']
+    }
+  },
+  methods: {
+    addToDo(content) {
+      this.todos.push(content) //클릭하면 여기로 전달됨
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ todoproject-dy/src/components/ToDoList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="todolist">
+      <div id="container" v-for="data in todos" :key="data">
+        <li id="text">{{ data }}</li>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      todos: Array
+    }
+  }
+  </script>
+  
+  <style></style>
+  
